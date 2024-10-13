ce169ecb7532baab57058fba72ca2bea4141b3e3
Muhammad Fahreza <yya77591@gmail.com>
2024-10-13 13:25:18 +0700

    Aplikasi Event

--- app/src/main/java/com/example/aplikasievent/Event.kt
+++ app/src/main/java/com/example/aplikasievent/Event.kt
@@ -3,5 +3,11 @@ package com.example.aplikasievent
 data class Event(
     val id: Int,
     val name: String,
-    val imageUrl: String
+    val imageUrl: String,
+    val ownerName: String,
+    val beginTime: String,
+    val quota: Int,
+    val registrant: Int,
+    val description: String,
+    val link: String
 )
